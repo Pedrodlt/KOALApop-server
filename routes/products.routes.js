84842ba2be21a76b02387e595cbe4b0608b9154e
Pedro de la Torre <pedrodltl@gmail.com>
2023-05-30 15:58:55 +0200

    gracias jaime

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router()
 const { getAllProducts, getOneProduct, saveProduct } = require("../controllers/product.controllers")
 
 router.get("/getAllProducts", getAllProducts)
-router.get("/getOneProduct/:products_id", getOneProduct)
+router.get("/getOneProduct/:product_id", getOneProduct)
 router.post("/saveProduct", saveProduct)
 
 module.exports = router
