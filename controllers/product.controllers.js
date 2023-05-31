2204ea6f6e1b13c57487df60df8cdc8db58a2fe4
Gustavo Gesto <gestogarciagustavo@gmail.com>
2023-05-31 15:27:56 +0200

    123456789

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -5,7 +5,7 @@ const getAllProducts = (req, res, next) => {
 
     Product
         .find()
-        .select({ title: 1, image: 1 })
+        .select({ title: 1, image: 1, owner: 1 })
         .sort({ title: 1 })
         .then(response => setTimeout(() => res.json(response), 1000))
         .catch(err => next(err))
@@ -26,9 +26,10 @@ const getOneProduct = (req, res, next) => {
 const saveProduct = (req, res, next) => {
 
     const { title, description, category, price, image } = req.body
+    const { _id: owner } = req.payload
 
     Product
-        .create({ title, description, category, price, image })
+        .create({ title, description, category, price, image, owner })
         .then(response => res.json(response))
         .catch(err => next(err))
 }
@@ -39,18 +40,23 @@ const editProduct = (req, res, next) => {
     const { product_id } = req.params
     const { title, description, category, price } = req.body
 
-    if (req.file) {
-        const { path: image } = req.file
-        Product
-            .findByIdAndUpdate(product_id, { title, description, category, price, image })
-            .then(response => res.json(response))
-            .catch(err => next(err))
-    } else {
-        Product
-            .findByIdAndUpdate(product_id, { title, description, category, price })
-            .then(response => res.json(response))
-            .catch(err => next(err))
-    }
+    Product
+        .findByIdAndUpdate(product_id, { title, description, category, price, image })
+        .then(response => res.json(response))
+        .catch(err => next(err))
+
+    // if (req.file) {
+    //     const { path: image } = req.file
+    //     Product
+    //         .findByIdAndUpdate(product_id, { title, description, category, price, image })
+    //         .then(response => res.json(response))
+    //         .catch(err => next(err))
+    // } else {
+    //     Product
+    //         .findByIdAndUpdate(product_id, { title, description, category, price })
+    //         .then(response => res.json(response))
+    //         .catch(err => next(err))
+    // }
 }
 
 // PRODUCT DELETE
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router()
 
 const { getAllProducts, getOneProduct, saveProduct, editProduct, deleteProduct } = require("../controllers/product.controllers")
+const { isAuthenticated } = require("../middlewares/verifyToken.middleware")
 
 router.get("/getAllProducts", getAllProducts)
 router.get("/getOneProduct/:product_id", getOneProduct)
-router.post("/saveProduct", saveProduct)
-router.put("/editProduct", editProduct)
+router.post("/saveProduct", isAuthenticated, saveProduct)
+router.put("/editProduct", isAuthenticated, editProduct)
 router.delete("/deleteProduct", deleteProduct)
 
 module.exports = router
