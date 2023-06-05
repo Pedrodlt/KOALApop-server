81b597264da4ff185c0c7f136e6aeef15fa1bd3d
Pedro de la Torre <pedrodltl@gmail.com>
2023-06-05 12:01:15 +0200

    vaaaaaaaaa

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -11,5 +11,21 @@ router.post('/image', uploaderMiddleware.single('imageData'), (req, res) => {
     res.json({ cloudinary_url: req.file.path })
 })
 
+router.post('/images', uploaderMiddleware.array('imagesData'), (req, res) => {
+
+    if (!req.files || req.files.length === 0) {
+        res.status(500).json({ errorMessage: 'Error caragndo el archivo' })
+        return
+    }
+
+    const urls = req.files.map(file => file.path)
+
+    res.json({ cloudinary_urls: urls })
+
+    // console.log('LAS FILES', req.files)
+
+    // res.json({ cloudinary_url: req.files.path })
+})
+
 
 module.exports = router
