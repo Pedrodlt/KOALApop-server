397eedc6aa8acef725110b7f27542c43e9965119
Pedro de la Torre <pedrodltl@gmail.com>
2023-06-05 10:12:45 +0200

    pues eso

--- middlewares/uploader.middleware.js
+++ middlewares/uploader.middleware.js
@@ -11,3 +11,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({ cloudinary })
 
 module.exports = multer({ storage })
+
+// const upload = multer({ storage }).array('imagenes', 5)  // 'imagenes' es el nombre del campo de entrada en el formulario y 5 es el límite de archivos
+
+// module.exports = upload
